6cb3dd5d1e42d4c312aba70e4548847a235efdfd
inkuksuss <inkuksuss@naver.com>
2023-11-21 18:01:59 +0900

    cors 변경

--- src/main/java/com/catpaw/catpawmiddleware/config/WebConfig.java
+++ src/main/java/com/catpaw/catpawmiddleware/config/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://api.my-pooding.com")
+                .allowedOrigins("https://www.my-pooding.com")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
